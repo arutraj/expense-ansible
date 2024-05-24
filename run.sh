3921900774247bc01209443044211710b05b0ace
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-24 08:38:17 +0530

    Use vault token

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ if [ "$pipeline" == "true" ]; then
   exit
 fi
 ##
-ansible-playbook -i ${component}-${env}.rdevopsb79.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=dev expense.yml
+ansible-playbook -i ${component}-${env}.rdevopsb79.online, -e ansible_user=ec2-user -e ansible_password=${SSH_PASSWORD} -e role_name=${component} -e env=${env} -e vault_token=${vault_token} expense.yml
 
